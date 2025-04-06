751ccbbbf9924a0e208a656f8622b2f4f6230b4a
kanat <esenalievkanat.90@gmail.com>
2025-04-07 05:37:27 +0600

    Добавлен DTO для передачи данных о категориях. Реализованы: поля id и name с использованием паттерна Builder от Lombok для упрощения создания объектов

--- src/main/java/org/example/labwork55/dto/CategoryDto.java
+++ src/main/java/org/example/labwork55/dto/CategoryDto.java
@@ -1,4 +1,15 @@
 package org.example.labwork55.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
+    private Integer id;
+    private String name;
 }
