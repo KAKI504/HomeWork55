39f09e92867c862e93d0691683ab1e047a9637ea
kanat <esenalievkanat.90@gmail.com>
2025-04-07 05:37:18 +0600

    Добавлена модель Category для работы с категориями викторин. Реализованы: поля id и name, конструкторы, геттеры/сеттеры с использованием Lombok

--- src/main/java/org/example/labwork55/model/Category.java
+++ src/main/java/org/example/labwork55/model/Category.java
@@ -1,4 +1,15 @@
 package org.example.labwork55.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    private int id;
+    private String name;
 }
